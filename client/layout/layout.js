b4653895b52f0cdaecc476384f6892b8cda612a7
wmitchel <williamjamesmitchel@gmail.com>
2016-04-28 13:20:46 -0400

    Replace game displays with collapsable accordion. Remove css file. Fix navbar-brand font size issue. Change Font.

--- client/layout/layout.js
+++ client/layout/layout.js
@@ -13,19 +13,14 @@ Template.layout.events({
 
 Template.nav.helpers({
     'notifications': function(){
-        console.log("three");
         let foundUser = Meteor.users.findOne({_id: Meteor.userId()});
         let name = foundUser.username;
-        console.log("four");
         return Notifications.find({ $and: [ { recieverUsername: name }, { read: false } ] });
     },
     'numNotifications': function(){
-        console.log("One");
         let foundUser = Meteor.users.findOne({_id: Meteor.userId()});
         let name = foundUser.username;
         let numNotif = Notifications.find({ $and: [ { recieverUsername: name }, { read: false } ] }).count();
-        console.log(numNotif);
-        console.log("Two");
         if (isNaN(numNotif)){
             return "Not a number";
         } else {
