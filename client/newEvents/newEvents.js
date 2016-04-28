5d409790c5182ed92b0015e0436bc3afe8c5c74d
wmitchel <williamjamesmitchel@gmail.com>
2016-04-28 19:18:33 -0400

    Add front end form validation for new events.

--- client/newEvents/newEvents.js
+++ client/newEvents/newEvents.js
@@ -7,8 +7,7 @@ Template.newEvent.onRendered(function() {
             // Create the autocomplete object, restricting the search to geographical
             // location types.
             autocomplete = new google.maps.places.Autocomplete(
-                /** @type {!HTMLInputElement} */(document.getElementById('autocomplete')),
-                {types: ['geocode']});
+                /** @type {!HTMLInputElement} */(document.getElementById('autocomplete')));
 
                 // When the user selects an address from the dropdown, populate the address
                 // fields in the form.
@@ -52,6 +51,7 @@ Template.newEvent.onRendered(function() {
                 })
             }
 
+            $("#newEventForm").validate();
             initAutocomplete();
 });
 
